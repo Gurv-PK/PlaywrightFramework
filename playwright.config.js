91ba70ce09b4517ad46a08ef5333cae0f49d07b6
Gurv-PK <gp41915@gmail.com>
2026-01-31 00:05:50 +0530

    Complete Jenkins Integration - Trail run 1

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
         forbidOnly: !!process.env.CI,
         retries: process.env.CI ? 2 : 0,
         workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
+    reporter: [['html', {open: "never"}]],
 
     use: {
         baseURL: ENV_CONFIG.url,
